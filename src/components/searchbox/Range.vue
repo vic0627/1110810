3632db6cbfacf4c9346993758d7e16084097fd15
DESKTOP-GMTST5N\Sunrise <pure90719@gmail.com>
2023-03-21 17:29:18 +0800

    add range

--- src/components/searchbox/Range.vue
+++ src/components/searchbox/Range.vue
@@ -1,19 +1,49 @@
 <script setup>
-import { ref } from 'vue'
-
-
+import { computed } from 'vue'
+import userStore from '../../stores/user';
+const user = userStore();
+const inputs = computed(() => [
+    {
+        label: '行政區',
+        type: 'option',
+        store: user.getDis,
+        placeholder: '請選擇行政區'
+    },
+    {
+        label: '巡查時間',
+        type: 'date',
+    },
+])
 </script>
 
 <template>
     <div class="range">
-        range
+        <div class="inputWrap" v-for="(e, index) in inputs" :key="index">
+            <label :for="e.type">{{ e.label }}</label>
+            <select v-if="e.type === 'option'" :id="e.type">
+                <option value="" disabled selected>{{ `請選擇${e.label}` }}</option>
+                <option :value="item" v-for="item in e.store" :key="item">{{ item }}</option>
+            </select>
+            <input v-else :type="e.type" :id="e.type">
+        </div>
+        <button class="confirmSearch">查詢</button>
     </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .range {
     width: 352px;
     height: 352px;
     border: 1px solid #707070;
+    >button {
+        width: 320px;
+        height: 48px;
+        background: #02318E;
+        border-radius: 4px;
+        border: none;
+        color: #fff;
+        font-size: 24px;
+        cursor: pointer;
+    }
 }
 </style>
